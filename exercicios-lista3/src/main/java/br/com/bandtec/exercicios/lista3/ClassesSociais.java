1f4d2ab591b4255a842e4ccf33008d44f88f2438
Matheus Franciscone <matheus.cunha@bandtec.com.br>
2021-08-27 14:33:08 -0300

    Methods. A new saga!

---
+++ exercicios-lista3/src/main/java/br/com/bandtec/exercicios/lista3/ClassesSociais.java
@@ -0,0 +1,16 @@
+package br.com.bandtec.exercicios.lista3;
+
+import java.util.Scanner;
+
+public class ClassesSociais {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        DefineClasse define = new DefineClasse();
+        
+        System.out.println("Digite sua renda:");
+        Integer rendaSM = leitor.nextInt();
+        
+        
+        
+    }
+}
---
+++ exercicios-lista3/src/main/java/br/com/bandtec/exercicios/lista3/DefineClasse.java
@@ -0,0 +1,5 @@
+package br.com.bandtec.exercicios.lista3;
+
+public class DefineClasse {
+
+}
